cddba22aaa5d894a901c53374fab070704d14e4d
YoshinoriN <nayo-msoft@outlook.jp>
2020-10-03 23:27:46 +0900

    feat: return 404 when not exist user

--- cahsper/routes/users.py
+++ cahsper/routes/users.py
@@ -6,6 +6,10 @@ module_users = Blueprint('users', __name__)
 @module_users.route("/users/<user_name>", methods=['GET'])
 def get_user(user_name):
     user = Users.find_by_name(user_name)
+
+    if not user:
+        return jsonify({"message": "NotFound"})
+
     return jsonify(user)
 
 @module_users.route("/users", methods=['GET'])
